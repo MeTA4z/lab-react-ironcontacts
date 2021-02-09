70f625fcd163f6d69e11adc89f4693f34041cd4e
Rafael Rodriguez <rafaelarodriguez1126@gmail.com>
2021-02-09 18:13:50 -0500

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App" >
-      </div>
-    );
+
+
+console.log(contacts)
+
+function App() {
+  const [stateCelebs, setStateCelebs] = useState (contacts.splice(0, 5))
+  const [otherCelebs, setOtherCelebs] = useState(contacts)
+  const showFive = () => {
+    return stateCelebs.map(eachContact => {
+      return <li>{eachContact.name} <img src= {eachContact.pictureUrl}/></li>
+    })
   }
+
+const addRandom = () => {
+  console.log('hello')
+  let randomNum = Math.floor(Math.random() * otherCelebs.length)
+  let randomCelebs = otherCelebs.splice(randomNum,
+  sth(otherCelebs.splice(randomNum, 1)[0]
+ 
+  newCelebs.push({ ...randomCeleb })
+
+  setStateCelebs(newCelebs)
+}
+
+
+  return (
+    <div>Hello
+    {showFive()}
+    <button onClick={addRandom}>Add Random Actor</button>
+    </div>
+  )
 }
 
 export default App;
+
+
+
+
+
+
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App" >
+//       This is fun
+//       </div>
+//     );
+//   }
+// }
